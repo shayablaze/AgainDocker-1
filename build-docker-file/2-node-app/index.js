354f02b7af83e099f2758e469e48edc0a0f1c429
Ajzner, Yeshayahu Dov <YeshayahuDov.Ajzner@ca.com>
2019-04-03 08:52:57 +0300

    r

--- build-docker-file/2-node-app/index.js
+++ build-docker-file/2-node-app/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('How are you doing1111');
+  res.send('How are you man');
 });
 
 app.listen(8080, () => {
