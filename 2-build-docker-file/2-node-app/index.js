61633e8577ea2033f2a828bfadae99b3661fc217
shayablaze <YeshayahuDov.Ajzner@ca.com>
2019-12-17 19:39:36 +0200

    gr

--- 2-build-docker-file/2-node-app/index.js
+++ 2-build-docker-file/2-node-app/index.js
@@ -2,10 +2,21 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('How are you man');
+const ONE_SECOND = 1000
+const FIVE_SECONDS = 5000
+
+app.get('/operation-1', (req, res) => {
+  res.status(400).send('Got an error');
 });
 
-app.listen(8080, () => {
-  console.log('Listening on port 8080');
+app.get('/operation-2', (req, res) => {
+  setTimeout(function (){ res.send('Got very bad performance, 5 seconds')}, FIVE_SECONDS);
+});
+
+app.get('/operation-3', (req, res) => {
+  setTimeout(function (){ res.send('Got good performance, 1 second')}, ONE_SECOND);
+});
+
+app.listen(80, () => {
+  console.log('Listening on port 80');
 });
