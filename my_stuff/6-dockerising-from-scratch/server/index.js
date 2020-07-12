5807d9a29d39f0c6dea7c01b4c9ca6fedc9037e1
shayablaze <YeshayahuDov.Ajzner@ca.com>
2020-07-12 17:23:20 +0300

    sdfesfwef

--- my_stuff/6-dockerising-from-scratch/server/index.js
+++ my_stuff/6-dockerising-from-scratch/server/index.js
@@ -21,8 +21,8 @@ const pgClient = new Pool({
 
 pgClient.on('connect', () => {
   pgClient
-    .query("CREATE TABLE IF NOT EXISTS values (number INT)")
-    .catch((err) => console.log(err));
+      .query('CREATE TABLE IF NOT EXISTS values (number INT)')
+      .catch((err) => console.log(err));
 });
 
 // Redis Client Setup
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-  const values = await pgClient.query("SELECT * from values");
+  const values = await pgClient.query('SELECT * from values');
 
   res.send(values.rows);
 });
